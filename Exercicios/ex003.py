5f42e55337321b38311ca9738374590ffea1b2f7
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-05-03 14:32:12 -0300

    Update ex003.py
    Adicionando comentários

--- Exercicios/ex003.py
+++ Exercicios/ex003.py
@@ -1,17 +1,25 @@
 import os
 os.system('cls || clear')
 
+# Solicitando os dados do usuario
 primeiroNumero = int(input('Digite o primeiro numero: '))
 segundoNumero = int(input('Digite o segundo numero: '))
+
+# Somando os numeros
 soma = primeiroNumero + segundoNumero
+
+# Calculando o valor do produto
 produto = primeiroNumero * segundoNumero
 
+# Calculando a media
 media = soma / 2
 
+# Exibindo o resultado no terminal
 print(f'Media: {media}')
 print(f'soma: {soma}')
 print(f'Produto: {produto}')
 
+# Verificando qual é o maior e o menor numero
 if primeiroNumero > segundoNumero:
     print(f'O maior numero é: {primeiroNumero}')
 else:
