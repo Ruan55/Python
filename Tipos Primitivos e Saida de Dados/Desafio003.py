bdca71fa01bc706d735448929476c66bb1f12d66
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-05-03 17:04:45 -0300

    Add files via upload

---
+++ Tipos Primitivos e Saida de Dados/Desafio003.py
@@ -0,0 +1,5 @@
+print('=== DESAFIO 03 ===')
+primeiroNumero = int(input('Digite o primeiro numero: '))
+segundoNumero = int(input('Digite o segundo numero: '))
+resultado = primeiroNumero + segundoNumero
+print(f'A soma entre {primeiroNumero} + {segundoNumero} = {resultado}')
