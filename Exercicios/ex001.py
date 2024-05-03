afd1b082657ae2b2768e31881ac6c5eadb360eb5
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-05-03 14:28:38 -0300

    Update ex001.py
    Adicionando comentários

--- Exercicios/ex001.py
+++ Exercicios/ex001.py
@@ -1,13 +1,17 @@
 import os
 os.system('cls || clear')
 
+# Solicitando os dados do usuario
 nome = input('Qual é o seu nome?: ')
 idade = input('Qual é a sua idade?: ')
 nota1 = int(input('Digite a primeira nota: '))
 nota2 = int(input('Digite a segunda nota: '))
+# Somando as notas
 soma = nota1 + nota2
+# Calculando a média
 media = soma / 2
 
+# Exibindo o resultado no terminal
 print(f'Nome: {nome}')
 print(f'Idade: {idade}')
 print(f'Primeira nota: {nota1}')
